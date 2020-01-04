77d48728a5ad1e7330d4d758f482e69edfe21996
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-01-04 10:42:00 -0600

    add Campsite Info Component

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class CampsiteInfo extends Component {
+
+  render() {
+    if (this.props.campsite) {
+      return <div className="row"> </div>;
+    } else {
+      return <div></div>;
+    }
+  }
+}
+
+export default CampsiteInfo;
