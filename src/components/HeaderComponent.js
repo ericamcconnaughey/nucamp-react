9892223bb312e83bdd55c2be0a47d6b3352f4e08
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-01-20 14:53:49 -0600

    separate reducers for each part of the state in Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -15,7 +15,7 @@ class Header extends Component {
     };
     this.toggleNav = this.toggleNav.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
-    this.handdleLogin = this.handleLogin.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   toggleNav() {
