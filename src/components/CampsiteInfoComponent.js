0a3950589fc9fb7644246a775143fccd729a8636
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-01-22 21:49:27 -0600

    add Fetch to communicate with json-server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardBody, CardText, Breadcrumb,
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const minLength = len => val => val && (val.length >= len);
 const maxLength = len => val => !val || (val.length <= len);
@@ -106,7 +107,7 @@ function RenderCampsite({campsite}) {
       return (
         <div className="col-md-5 m-1">
           <Card>
-            <CardImg top src={campsite.image} alt={campsite.name} />
+            <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
             <CardBody>
               <CardText>{campsite.description}</CardText>
             </CardBody>
