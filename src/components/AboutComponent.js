6ca2eff481ce00edc580075ccda02d1b18befe95
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-01-25 14:47:39 -0600

    add animations to PartnerList in About Component

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { Fade, Stagger } from 'react-animation-components';
 
 
 function About(props) {
@@ -82,9 +83,11 @@ function PartnerList(props) {
 
     const partners = props.partners.partners.map(partner => {
         return (
-            <Media tag="li" key={partner.id}>
-                <RenderPartner partner={partner} />
-            </Media>
+            <Fade key={partner.id}>
+                <Media tag="li">
+                    <RenderPartner partner={partner} />
+                </Media>
+            </Fade>
         );
     });
 
@@ -99,7 +102,11 @@ function PartnerList(props) {
     } else {
         return (
             <div className="col mt-4">
-                <Media list>{partners}</Media>
+                <Media list>
+                    <Stagger in>
+                      {partners}
+                    </Stagger>
+                </Media>
             </div>
         )
     }
